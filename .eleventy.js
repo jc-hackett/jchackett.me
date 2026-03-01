3670875017b1a54e2152f1657828a051e90397c7
Jeremiah Hackett <jeremiahhackett.lcsw@gmail.com>
2026-03-01 11:21:27 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,4 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("styles.css");
-  return {
-    dir: {
-      input: ".",
-      output: "_site"
-    }
-  };
+  return { dir: { input: ".", output: "_site" } };
 };
